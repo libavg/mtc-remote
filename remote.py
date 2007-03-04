1da976b2a48c741d13acfca8035400c300b2e1bc
igor <igor@libavg.de>
2007-03-04 16:50:01 +0000

    Added finger display to remote control app.
    svn path=/trunk/mtc/remote/; revision=2001

--- remote.py
+++ remote.py
@@ -16,28 +16,48 @@ def onTouch():
     global HOST
     global PORT
     Event = Player.getCurEvent()
-    if Event.type == avg.MOUSEBUTTONDOWN:
+    if Event.type == avg.MOUSEBUTTONDOWN or Event.type == avg.TOUCHDOWN:
         Type = "down"
-    elif Event.type == avg.MOUSEBUTTONUP:
+    elif Event.type == avg.MOUSEBUTTONUP or Event.type == avg.TOUCHUP:
         Type = "up"
-    elif Event.type == avg.MOUSEMOTION:
+    elif Event.type == avg.MOUSEMOTION or Event.type == avg.TOUCHMOTION:
         Type = "motion"
     else:
         print Event.type
-    OSCController.sendMsg('/touch/'+Type, int(Event.cursorid), float(Event.x), float(Event.y))
+    OSCController.sendMsg('/touch/'+Type, int(Event.cursorid), 
+            float(Event.x), float(Event.y))
+
+def flipBitmap(Node):
+    for y in range(Node.getNumVerticesY()):
+        for x in range(Node.getNumVerticesX()):
+            pos = Node.getOrigVertexCoord(x,y)
+            pos.y = 1-pos.y
+            Node.setWarpedVertexCoord(x,y,pos)
+
+def onFrame():
+    global Tracker
+    Bitmap = Tracker.getImage(avg.IMG_FINGERS)
+    Node = Player.getElementByID("fingers")
+    Node.setBitmap(Bitmap)
+    Node.width=1280
+    Node.height=720
+    flipBitmap(Node)
 
 Player = avg.Player()
 Log = avg.Logger.get()
-Player.setResolution(0, 640, 0, 0) 
+#Player.setResolution(0, 640, 0, 0) 
+Player.setResolution(1, 0, 0, 0) 
 Log.setCategories(Log.APP |
                   Log.WARNING | 
 #                  Log.PROFILE |
-                  Log.CONFIG | 
-                  Log.EVENTS 
+                  Log.CONFIG
+#                  Log.EVENTS 
 #                  Log.EVENTS2
                  )
 Player.loadFile("remote.avg")
 Player.setFramerate(60)
-Tracker = Player.addTracker("/dev/video1394/0", 30, "640x480_MONO8")
+Tracker = Player.addTracker("/dev/video1394/0", 60, "640x480_MONO8")
+Tracker.setDebugImages(False, True)
 OSCController = controller.Controller((HOST, PORT), verbose=True)
+Player.setInterval(1, onFrame)
 Player.play()
